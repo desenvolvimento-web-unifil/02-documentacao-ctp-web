06dda32b28761f4c7780cb97a458ac2222aad2c4
MaiconBortoli <89494572+MaiconBortoli@users.noreply.github.com>
2023-06-21 23:38:54 -0300

    Add files via upload
    tela de registro (html)

---
+++ siteAposta/resources/views/register/index.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap" rel="stylesheet">
+
+  <title>Tela de Registro</title>
+  <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
+</head>
+<body>
+  <div class="container">
+    <h2>TTL</h2>
+    <h3>Bem Vindo</h3>
+    <h4>Top Team League</h4>
+    <form>
+      <input class="user" type="text" placeholder="Usuário" required>
+      <input class="senha" type="password" placeholder="Senha" required>
+      <button type="submit">Registrar</button>
+    </form>
+  </div>
+</body>
+</html>
