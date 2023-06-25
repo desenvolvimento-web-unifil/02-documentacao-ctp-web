b7d743a7c36f6e0b7413e18b415996f59da7d926
Vi1ctor <82913507+Vi1ctor@users.noreply.github.com>
2023-06-25 17:34:02 -0300

    requisição ajax do novo saldo para controller

--- siteAposta/public/js/deposit.js
+++ siteAposta/public/js/deposit.js
@@ -1,3 +1,5 @@
+
+
 function increaseDeposit() {
     var depositValue = prompt("Digite o valor para aumentar o depósito:");
 
@@ -23,4 +25,26 @@ function increaseDeposit() {
         walletElementMobile.textContent =
             "R$" + newBalance.toFixed(2).replace(".", ",");
     }
+
+    updateBalanceDatabase(newBalance);
+}
+
+function updateBalanceDatabase(newBalance)
+{
+
+    axios.post('/atualizar-saldo', {
+        newBalance: newBalance
+      })
+      .then(function (response) {
+        // Sucesso na requisição
+        var saldoAtualizado = response.data.saldo;
+        
+        // Atualizar a exibição do saldo no frontend
+        document.getElementById("walletBalance").textContent = saldoAtualizado;
+        
+      })
+      .catch(function (error) {
+        // Tratamento de erro
+        console.log(error);
+      });
 }
