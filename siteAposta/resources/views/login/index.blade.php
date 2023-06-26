c0de116e2983f23edcb1529e3d7f2a308c94ed08
Neithan <82913980+NeithanGDT@users.noreply.github.com>
2023-06-26 00:00:17 -0300

    Update index.blade.php

--- siteAposta/resources/views/login/index.blade.php
+++ siteAposta/resources/views/login/index.blade.php
@@ -9,14 +9,14 @@
 
 <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap" rel="stylesheet">
 
-  <title>Tela de Login</title>
+  <title>Login</title>
   <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
  
 </head>
 <body>
   <div class="container">
     <h2>TTL</h2>
-    <h3>BEM-VINDO</h3>
+    <h3>SEJA BEM-VINDO</h3>
     <h4>Top Team League</h4>
 
     <form id="form-login" method="POST" action="/login">
@@ -34,8 +34,8 @@
     <div>
       <input id="email" name="email" class="email" type="text" placeholder="Email" required>
       <input id="password" name="password" type="password" placeholder="Senha" required>
-      <button style="margin-bottom: 10px" type="submit">Entrar</button>
-      <button> <a href="{{ route('register.create') }}" style="color: white" >Cadastrar</a></button>
+      <button style="margin-bottom: 10px" type="submit">LOGIN</button>
+      <button> <a href="{{ route('register.create') }}" style="color: white" >REGISTRAR</a></button>
     </div>  
     </form>
   </div>
